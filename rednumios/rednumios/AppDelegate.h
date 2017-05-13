1fe3bc6229ffba72389a45ab2ab5b5cd2f698363
tanzhuo <zhuo_tan@sina.cn>
2017-05-13 14:34:37 +0800

    first code commit;

---
+++ rednumios/rednumios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rednumios
+//
+//  Created by 谭卓 on 2017/5/13.
+//  Copyright © 2017年 谭卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rednumios/rednumios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rednumios
+//
+//  Created by 谭卓 on 2017/5/13.
+//  Copyright © 2017年 谭卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
